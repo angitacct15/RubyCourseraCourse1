362517434a6a15527a1cd9904148747928ad77ae
angitacct15 <etl.consultant7678@gmail.com>
2017-07-20 16:58:38 -0500

    added documentation to route.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   root 'courses#index'
 
+  # AN: the way we call this on browser is: ?looking_for=diet
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
